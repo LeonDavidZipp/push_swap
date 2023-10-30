4508d434a9586f4e72b131188cbae118829e02df
Leon David Zipp <leondavidzipp@gmx.de>
2023-10-30 23:42:24 +0100

    additional functionality

--- prepare_input.c
+++ prepare_input.c
@@ -3,28 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   prepare_input.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 15:34:45 by lzipp             #+#    #+#             */
-/*   Updated: 2023/10/30 15:39:52 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/10/30 23:40:13 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	prepare_input(int argc, char **argv)
+static char	*join_input(int argc, char **argv)
 {
 	int		i;
 	char	*num_string;
 	char	*temp;
-	char	*temp2;
 
 	i = 1;
 	num_string = NULL;
 	while (i < argc)
 	{
-		temp = ft_strdup(argv[i]);
-		if (num_string)
-			temp2 = ft_strjoin(num_string, temp);
+		temp = ft_strsepjoin(num_string, argv[i++], " ");
+		free (num_string);
+		if (!temp)
+			return (NULL);
+		num_string = ft_strdup(temp);
+		free (temp);
 	}
+	return (num_string);
+}
+
+int	*prepare_stack_a(int argc, char **argv)
+{
+	char	*num_string;
+	char	**num_strings;
+	int		height_a;
+
+	num_string = join_input(argc, argv);
+	if (!num_string)
+		return (NULL);
+	num_strings = ft_split(num_string, " ");
+	free (num_string);
+	if (!num_strings)
+		return (NULL);
+	height_a = 0;
+	while (num_strings[height_a] != NULL)
+		height_a++;
 }
