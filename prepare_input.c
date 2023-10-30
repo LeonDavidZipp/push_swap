f593854e33d14bc5e40956c031affae05d01551c
LeondavidZipp <leondavidzipp@gmx.de>
2023-10-30 15:39:56 +0100

    nothing special

---
+++ prepare_input.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prepare_input.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/30 15:34:45 by lzipp             #+#    #+#             */
+/*   Updated: 2023/10/30 15:39:52 by lzipp            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	prepare_input(int argc, char **argv)
+{
+	int		i;
+	char	*num_string;
+	char	*temp;
+	char	*temp2;
+
+	i = 1;
+	num_string = NULL;
+	while (i < argc)
+	{
+		temp = ft_strdup(argv[i]);
+		if (num_string)
+			temp2 = ft_strjoin(num_string, temp);
+	}
+}
