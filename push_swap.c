9dbacea18a03285e13e972fc634fca52b54385ef
LeondavidZipp <leondavidzipp@gmx.de>
2023-10-30 13:45:00 +0100

    fixed naming issues

--- push_swap.c
+++ push_swap.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 10:19:48 by lzipp             #+#    #+#             */
-/*   Updated: 2023/10/30 10:28:10 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/10/30 13:38:37 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	main(int argc, char **argv)
 {
 	// prevent wrong input
-	if (argc <= 1) // missing: letters, duplicates, other symbols
+	if (argc == 1) // missing: letters, duplicates, other symbols
 		return (0);
 	populate_stack_a(argv); // write input to stack_a
 	
