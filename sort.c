5c80a4f59b19a61473d5f326bd8c98dec6c0a7d1
LeondavidZipp <leondavidzipp@gmx.de>
2023-11-02 10:42:03 +0100

    nothing special

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 12:15:05 by lzipp             #+#    #+#             */
-/*   Updated: 2023/11/02 10:06:15 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/11/02 10:41:57 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,32 +32,43 @@ static int	count_runs(t_stack *stack_a)
 	return (count);
 }
 
+static void	move_run(t_stack *stack_a, t_stack *stack_b, int stack_flag)
+{
+	int		temp;
+	int		stack_flag;
+
+	temp = stack_a->stack[0];
+	while (temp < stack_a->stack[1])
+	{
+		if (stack_flag == 1)
+			ra_wrapper(stack_a->stack, stack_a->height);
+		else if (stack_flag == -1)
+		{
+			pb_wrapper(stack_a, stack_b,
+				&stack_a->height, &stack_b->height);
+			rb_wrapper(stack_b->stack, stack_b->height);
+		}
+		temp = stack_a->stack[0];
+	}
+}
+
 /// @brief Distributes the runs evenly between stack_a and stack_b.
 /// @param stack_a 
 /// @param stack_b 
 static void	distribute_runs(t_stack *stack_a, t_stack *stack_b)
 {
-	int		temp;
 	int		stack_flag;
-	int		runs;
+	int		runs_a;
+	int		runs_b;
 
 	stack_flag = 1;
-	runs = count_runs(stack_a);
-	while (1)
+	runs_a = count_runs(stack_a);
+	runs_b = 0;
+	while (runs_a != runs_b && runs_a != runs_b + 1 && runs_a != runs_b - 1)
 	{
-		temp = stack_a->stack[0];
-		while (temp < stack_a->stack[1])
-		{
-			if (stack_flag == 1)
-				ra_wrapper(stack_a->stack, stack_a->height);
-			else if (stack_flag == -1)
-			{
-				pb_wrapper(stack_a, stack_b,
-					&stack_a->height, &stack_b->height);
-				rb_wrapper(stack_b->stack, stack_b->height);
-			}
-			temp = stack_a->stack[0];
-		}
+		move_run(stack_a, stack_b, stack_flag);
+		runs_a--;
+		runs_b++;
 		stack_flag *= -1;
 	}
 }
@@ -76,6 +87,7 @@ void	sort(t_stack *stack_a, t_stack *stack_b)
 	}
 	while (stack_a->stack[0] > stack_a->stack[stack_a->height - 1])
 		ra_wrapper(stack_a->stack, stack_a->height);
+	distribute_runs(stack_a, stack_b);
 }
 
 #include <stdio.h>
