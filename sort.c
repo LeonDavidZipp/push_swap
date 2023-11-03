f479df4da68a329a2053eaec385176c5101921be
Leon David Zipp <leondavidzipp@gmx.de>
2023-11-03 09:58:44 +0100

    no noteworthy changes

--- sort.c
+++ sort.c
@@ -6,26 +6,50 @@
 /*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 10:53:15 by lzipp             #+#    #+#             */
-/*   Updated: 2023/11/03 09:31:32 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/11/03 09:57:04 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// is actually merge to b
 static void	merge_to_a(t_stack *stack_a, t_stack *stack_b)
 {
-	if (stack_a->stack[0] < stack_b->stack[0])
+	if (stack_a->stack[0] > stack_b->stack[0])
+	{
+		pa_wrapper(stack_a->stack, stack_b->stack,
+			&(stack_a->height), &(stack_b->height));
+		ra_wrapper(stack_a->stack, stack_a->height);
+	}
+	else if (stack_a->stack[0] < stack_b->stack[0])
+		ra_wrapper(stack_a->stack, stack_a->height);
+	while (stack_a->stack[stack_a->height - 1] <= stack_a->stack[0])
+	{
+		if (stack_a->stack[0] > stack_b->stack[0])
+			pa_wrapper(stack_a->stack, stack_b->stack,
+				&(stack_a->height), &(stack_b->height));
+		ra_wrapper(stack_a->stack, stack_a->height);
+	}
+	while (stack_a->stack[stack_a->height - 1] <= stack_b->stack[0])
+	{
+		pa_wrapper(stack_a->stack, stack_b->stack,
+			&(stack_a->height), &(stack_b->height));
+		ra_wrapper(stack_a->stack, stack_a->height);
+	}
+}
+
+static void	merge_to_b(t_stack *stack_a, t_stack *stack_b)
+{
+	if (stack_b->stack[0] > stack_a->stack[0])
 	{
 		pb_wrapper(stack_a->stack, stack_b->stack,
 			&(stack_a->height), &(stack_b->height));
 		rb_wrapper(stack_b->stack, stack_b->height);
 	}
-	if (stack_a->stack[0] > stack_b->stack[0])
+	else if (stack_b->stack[0] < stack_a->stack[0])
 		rb_wrapper(stack_b->stack, stack_b->height);
 	while (stack_b->stack[stack_b->height - 1] <= stack_b->stack[0])
 	{
-		if (stack_a->stack[0] < stack_b->stack[0])
+		if (stack_b->stack[0] > stack_a->stack[0])
 			pb_wrapper(stack_a->stack, stack_b->stack,
 				&(stack_a->height), &(stack_b->height));
 		rb_wrapper(stack_b->stack, stack_b->height);
@@ -38,28 +62,6 @@ static void	merge_to_a(t_stack *stack_a, t_stack *stack_b)
 	}
 }
 
-static void	merge_to_b(t_stack *stack_b, t_stack *stack_a)
-{
-	if (stack_b->stack[0] < stack_a->stack[0])
-	{
-		pa_wrapper(stack_b->stack, stack_a->stack,
-			&(stack_b->height), &(stack_a->height));
-		ra_wrapper(stack_a->stack, stack_a->height);
-	}
-	while (stack_a->stack[stack_a->height - 1] <= stack_a->stack[0])
-	{
-		if (stack_b->stack[0] < stack_a->stack[0])
-			pa_wrapper(stack_b->stack, stack_a->stack,
-				&(stack_b->height), &(stack_a->height));
-		ra_wrapper(stack_a->stack, stack_a->height);
-	}
-	while (stack_a->stack[stack_a->height - 1] <= stack_b->stack[0])
-	{
-		pa_wrapper(stack_b->stack, stack_a->stack,
-			&(stack_b->height), &(stack_a->height));
-		ra_wrapper(stack_a->stack, stack_a->height);
-	}
-}
 #include<stdio.h>
 /// @brief Sorts stack_a using stack_b as a temporary stack.
 /// @param stack_a 
@@ -79,10 +81,8 @@ void	sort(t_stack *stack_a, t_stack *stack_b)
 	stack_flag = 1;
 	while (stack_b->height > 0)
 	{
-		// if (stack_flag == 1)
 		merge_to_a(stack_a, stack_b);
-		// else
-		// merge_to_b(stack_a, stack_b);
+		merge_to_b(stack_a, stack_b);
 		printf("stack A: ");
 		for (int i = 0; i < stack_a->height; i++)
 			printf("%d ", stack_a->stack[i]);
