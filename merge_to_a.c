36b9ec5d164abb90682f767e92e905406b38a93d
LeondavidZipp <leondavidzipp@gmx.de>
2023-11-10 15:12:53 +0100

    nothing special

--- merge_to_a.c
+++ merge_to_a.c
@@ -6,14 +6,14 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 17:37:30 by lzipp             #+#    #+#             */
-/*   Updated: 2023/11/10 12:55:10 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/11/10 15:11:44 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-void	merge_to_a(t_stack *stack_a, t_stack *stack_b)
+void	merge_to_a_old(t_stack *stack_a, t_stack *stack_b)
 {
 	// while there is an ongoing reverse run in B and B[0] is comparable to B[1]
 	int		temp_a;
@@ -23,9 +23,10 @@ void	merge_to_a(t_stack *stack_a, t_stack *stack_b)
 	temp_b = stack_b->stack[0];
 	while (stack_b->height >= 1 && temp_b >= stack_b->stack[0] && temp_a <= stack_a->stack[0])
 	{
-		printf("temp_a: %d, temp_b: %d, b0: %d, b1: %d\n", temp_a, temp_b, stack_a->stack[0], stack_b->stack[1]);
-		temp_a = stack_a->stack[0];
+		printf("before:\n");
+		printf("temp_a: %d\ntemp_b: %d\na0: %d\nb0: %d, b1: %d\n", temp_a, temp_b, stack_a->stack[0], stack_b->stack[0], stack_b->stack[1]);
 		temp_b = stack_b->stack[0];
+		temp_a = stack_a->stack[0];
 		// push B to A if B[0] > A[0]
 		if (stack_a->stack[0] > stack_b->stack[0])
 			pa_wrapper(stack_a->stack, stack_b->stack,
@@ -33,14 +34,17 @@ void	merge_to_a(t_stack *stack_a, t_stack *stack_b)
 		// else rotate A and try again
 		// else
 		// write(1, "am here now first\n", 18);
-		ra_wrapper(stack_a->stack, stack_a->height);
+		else
+			ra_wrapper(stack_a->stack, stack_a->height);
+		printf("after:\n");
+		printf("temp_a: %d\ntemp_b: %d\na0: %d\nb0: %d, b1: %d\n", temp_a, temp_b, stack_a->stack[0], stack_b->stack[0], stack_b->stack[1]);
+		printf("stack_a:");
 		for (int i = 0; i < stack_a->height; i++) {
-        printf("%d, ", stack_a->stack[i]);
-    }
-	// printf("hi");
-    printf("\n");
+			printf("%d, ", stack_a->stack[i]);
+		}
+	printf("\n------------------\n");
 	}
-	write(1, "am here\n", 8);
+	// write(1, "am here\n", 8);
 	// if height B is 1, rotate A until A[0] > B[0] or A[0] > A[1]
 	// 		if A[0] > B[0] push B to A
 	// 		else if A[0] > A[1], rotate A once more, then push B to A
@@ -48,8 +52,8 @@ void	merge_to_a(t_stack *stack_a, t_stack *stack_b)
 	{
 		while (stack_a->stack[0] < stack_b->stack[0] && stack_a->stack[0] < stack_a->stack[1])
 		{
-			printf("a0: %d, a1: %d, b0: %d\n", stack_a->stack[0], stack_a->stack[1], stack_b->stack[0]);
-			write(1, "am here now loop\n", 17);
+			// printf("a0: %d, a1: %d, b0: %d\n", stack_a->stack[0], stack_a->stack[1], stack_b->stack[0]);
+			// write(1, "am here now loop\n", 17);
 			ra_wrapper(stack_a->stack, stack_a->height);
 		}
 		if (stack_a->stack[0] > stack_b->stack[0])
@@ -60,7 +64,7 @@ void	merge_to_a(t_stack *stack_a, t_stack *stack_b)
 			pa_wrapper(stack_a->stack, stack_b->stack, &(stack_a->height), &(stack_b->height));
 		}
 	}
-	write(1, "am here now\n", 12);
+	// write(1, "am here now\n", 12);
 	temp_a = stack_a->stack[0];
 	while (temp_a <= stack_a->stack[0])
 	{
@@ -74,6 +78,41 @@ void	merge_to_a(t_stack *stack_a, t_stack *stack_b)
 	
 }
 
+void	merge_to_a(t_stack *stack_a, t_stack *stack_b)
+{
+	int		temp_a;
+	int		temp_b;
+
+	temp_b = stack_b->stack[0];
+	temp_a = stack_a->stack[0];
+	printf("0th cond: %d\n", stack_b->height >= 1);
+	printf("first cond: %d\n", temp_b >= stack_b->stack[0]);
+	printf("second cond: %d\n", temp_a <= stack_a->stack[0]);
+	// return ;
+	while (stack_b->height >= 1 && temp_b >= stack_b->stack[0] && temp_a <= stack_a->stack[0])
+	{
+		temp_b = stack_b->stack[0];
+		if (stack_a->stack[0] > stack_b->stack[0])
+			pa_wrapper(stack_a->stack, stack_b->stack,
+				&(stack_a->height), &(stack_b->height));
+		else if (stack_a->stack[0] > stack_a->stack[1] && stack_a->stack[0] < stack_b->stack[0])
+		{
+			ra_wrapper(stack_a->stack, stack_a->height);
+			pa_wrapper(stack_a->stack, stack_b->stack,
+				&(stack_a->height), &(stack_b->height));
+		}
+		ra_wrapper(stack_a->stack, stack_a->height);
+		temp_a = stack_a->stack[0];
+	}
+	// to rotate run to the bottom. necessary because it is more efficient to merge runs of similar length
+	temp_a = stack_a->stack[0];
+	while (temp_a <= stack_a->stack[0])
+	{
+		// write(1, "am here now loop\n", 17);
+		temp_a = stack_a->stack[0];
+		ra_wrapper(stack_a->stack, stack_a->height);
+	}
+}
 /*
 Merging run A to B means the following:
 As long as the top of A is less than the top of B, push A to B. Then rotate B.
@@ -93,7 +132,6 @@ If there is only one element in A, rotate B until B > A OR B[0] > B[1], then pus
 // 			pa_wrapper(stack_a->stack, stack_b->stack,
 // 				&(stack_a->height), &(stack_b->height));
 // 		ra_wrapper(stack_a->stack, stack_a->height);
-		
 // 	}
 // }
 
@@ -103,12 +141,12 @@ int main() {
 
     // Initialize the heights of the stacks
     stack_a->height = 5;
-    stack_b->height = 4;
+    stack_b->height = 3;
 
     // Initialize the stacks with some values
-    int stack_a_values[12] = {10, 100, 300, 2, 1, 0, 0 ,0 ,0 ,0 ,0 ,0};
+    int stack_a_values[12] = {10, 100, 600, 2, 1, 0, 0 ,0 ,0 ,0 ,0 ,0};
     // int stack_b_values[] = {-3, 111, 308, 700, 6};
-    int stack_b_values[12] = {666, 200, 111, 420, 0, 0, 0, 0, 0, 0, 0};
+    int stack_b_values[12] = {601, 420, 3, 0, 0, 0, 0, 0, 0, 0, 0};
     stack_a->stack = stack_a_values;
     stack_b->stack = stack_b_values;
 
