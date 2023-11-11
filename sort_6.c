786ce59b20139781cb567058bfed6683f3488751
LeondavidZipp <leondavidzipp@gmx.de>
2023-11-11 14:08:37 +0100

    file cleanup

--- sort_6.c
+++ sort_6.c
@@ -6,12 +6,76 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 21:42:59 by lzipp             #+#    #+#             */
-/*   Updated: 2023/11/11 13:11:10 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/11/11 14:07:45 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
+
+static void	sort_2_a(t_stack *stack_a)
+{
+	if (stack_a->height <= 1)
+		return ;
+	if (stack_a->stack[0] > stack_a->stack[1])
+		sa_wrapper(stack_a->stack, stack_a->height);
+}
+
+static void	sort_2_b(t_stack *stack_b)
+{
+	if (stack_b->height <= 1)
+		return ;
+	if (stack_b->stack[0] > stack_b->stack[1])
+		sb_wrapper(stack_b->stack, stack_b->height);
+}
+
+static void	sort_3_a(t_stack *stack_a)
+{
+	if (stack_a->height <= 2)
+	{
+		sort_2_a(stack_a);
+		return ;
+	}
+	if (stack_a->stack[0] > stack_a->stack[1]
+		&& stack_a->stack[2] > stack_a->stack[1]
+		&& stack_a->stack[0] > stack_a->stack[2])
+	{
+		ra_wrapper(stack_a->stack, stack_a->height);
+		return ;
+	}
+	if (stack_a->stack[0] > stack_a->stack[1])
+		sa_wrapper(stack_a->stack, stack_a->height);
+	if (stack_a->stack[1] > stack_a->stack[2])
+		rra_wrapper(stack_a->stack, stack_a->height);
+	if (stack_a->stack[0] > stack_a->stack[1])
+		sa_wrapper(stack_a->stack, stack_a->height);
+}
+
+static void	sort_3_b(t_stack *stack_b)
+{
+	if (stack_b->height <= 2)
+	{
+		sort_2_b(stack_b);
+		return ;
+	}
+	if (stack_b->stack[0] < stack_b->stack[1]
+		&& stack_b->stack[1] < stack_b->stack[2])
+		return ;
+	if (stack_b->stack[0] > stack_b->stack[1]
+		&& stack_b->stack[2] > stack_b->stack[1]
+		&& stack_b->stack[0] > stack_b->stack[2])
+	{
+		rb_wrapper(stack_b->stack, stack_b->height);
+		return ;
+	}
+	if (stack_b->stack[0] > stack_b->stack[1])
+		sb_wrapper(stack_b->stack, stack_b->height);
+	if (stack_b->stack[1] > stack_b->stack[2])
+		rrb_wrapper(stack_b->stack, stack_b->height);
+	if (stack_b->stack[0] > stack_b->stack[1])
+		sb_wrapper(stack_b->stack, stack_b->height);
+}
+
 void	sort_6(t_stack *stack_a, t_stack *stack_b)
 {
 	if (stack_a->height > 3)
