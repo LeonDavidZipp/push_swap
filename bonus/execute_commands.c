c2c87d85d4515cfe1e4bba4c5aaf4c501df65c8b
LeondavidZipp <leondavidzipp@gmx.de>
2023-12-05 15:06:39 +0100

    cleaned up code

--- bonus/execute_commands.c
+++ bonus/execute_commands.c
@@ -6,31 +6,28 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 16:09:30 by lzipp             #+#    #+#             */
-/*   Updated: 2023/12/05 13:01:58 by lzipp            ###   ########.fr       */
+/*   Updated: 2023/12/05 15:04:19 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./checker.h"
 
-void	rr_wrapper(int *st_a, int *st_b, int h_a, int h_b)
+void	rr(int *st_a, int *st_b, int h_a, int h_b)
 {
 	ra(st_a, h_a);
 	rb(st_b, h_b);
-	write(1, "rr\n", 3);
 }
 
-void	rrr_wrapper(int *st_a, int *st_b, int h_a, int h_b)
+void	rrr(int *st_a, int *st_b, int h_a, int h_b)
 {
 	rra(st_a, h_a);
 	rrb(st_b, h_b);
-	write(1, "rrr\n", 4);
 }
 
-void	ss_wrapper(int *st_a, int *st_b, int h_a, int h_b)
+void	ss(int *st_a, int *st_b, int h_a, int h_b)
 {
 	sa(st_a, h_a);
 	sb(st_b, h_b);
-	write(1, "ss\n", 3);
 }
 
 static int	execute_command(char *str, t_stack *st_a, t_stack *st_b)
@@ -40,7 +37,7 @@ static int	execute_command(char *str, t_stack *st_a, t_stack *st_b)
 	else if (ft_strncmp(str, "sb\n", 3) == 0)
 		sb(st_b->st, st_b->h);
 	else if (ft_strncmp(str, "ss\n", 3) == 0)
-		ss_wrapper(st_a->st, st_b->st, st_a->h, st_b->h);
+		ss(st_a->st, st_b->st, st_a->h, st_b->h);
 	else if (ft_strncmp(str, "pa\n", 3) == 0)
 		pa(st_a->st, st_b->st, &st_a->h, &st_b->h);
 	else if (ft_strncmp(str, "pb\n", 3) == 0)
@@ -50,13 +47,13 @@ static int	execute_command(char *str, t_stack *st_a, t_stack *st_b)
 	else if (ft_strncmp(str, "rb\n", 3) == 0)
 		rb(st_b->st, st_b->h);
 	else if (ft_strncmp(str, "rr\n", 3) == 0)
-		rr_wrapper(st_a->st, st_b->st, st_a->h, st_b->h);
+		rr(st_a->st, st_b->st, st_a->h, st_b->h);
 	else if (ft_strncmp(str, "rra\n", 4) == 0)
 		rra(st_a->st, st_a->h);
 	else if (ft_strncmp(str, "rrb\n", 4) == 0)
 		rrb(st_b->st, st_b->h);
 	else if (ft_strncmp(str, "rrr\n", 4) == 0)
-		rrr_wrapper(st_a->st, st_b->st, st_a->h, st_b->h);
+		rrr(st_a->st, st_b->st, st_a->h, st_b->h);
 	else
 		return (1);
 	return (0);
@@ -71,8 +68,6 @@ int	execute_commands(t_stack *st_a, t_stack *st_b)
 	{
 		free(command);
 		command = get_next_line(0);
-		if (ft_strncmp(command, "stop\n", 5) == 0)
-			break ;
 		if (execute_command(command, st_a, st_b) == 1)
 		{
 			free(command);
